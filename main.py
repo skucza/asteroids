a08c936eca9d8e0caafd14ef79092dd2a3258792
Piotr Skuczynski <piotr.skuczynski@vimn.com>
2025-02-09 23:54:42 +0100

    add tick of 60 (fps) and calculate delta time

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
