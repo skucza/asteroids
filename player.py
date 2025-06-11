d9c9c8667d185fcc5b11cdf7406d779c48176c22
Piotr Skuczynski <piotr.skuczynski@vimn.com>
2025-06-11 13:12:57 +0200

    add Player class representing the player triangle

---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+
+        print(f"Player triangle [{a}{b}{c}]")
+
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, WHITE, self.triangle(), 2)
